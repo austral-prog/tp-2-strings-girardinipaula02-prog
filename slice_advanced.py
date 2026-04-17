52bc9121403429748a28ecac7ecd3edad5e1c6d2
girardinipaula02-prog <girardinipaula.02@gmail.com>
2026-04-17 12:12:35 -0300

    Add files via upload

--- slice_advanced.py
+++ slice_advanced.py
@@ -2,8 +2,8 @@ def slice_advanced():
     """Lee un texto e imprime los caracteres desde la posición 4
     en adelante, tomando uno de cada dos (paso 2).
     """
-    texto = input("Ingrese un texto: ")
+    texto = input()
     print(texto[4::2])
 
-slice_advanced()
+#slice_advanced()
 
